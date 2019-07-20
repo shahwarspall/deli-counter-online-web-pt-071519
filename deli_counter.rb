3f774c8d87cc8f15cbb2bb1a1b0aa0dc09125246
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 04:57:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,9 +12,15 @@ if array.length == 0
     puts "The line is currently: #{line_array.join(" ")}"
   end
 end
-
-def take_a_number(katz_deli,name)
- if katz_deli.unshift(name)
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
-  elsif katz_deli == [1]
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
 end
