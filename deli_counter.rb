d830cdc1ec8e7040bfcb7dca728987bf12adad7f
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 05:01:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,7 +17,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 def now_serving(array)
-  if array.push
+  if array.empty?
     puts "There is nobody waiting to be served!"
       else
     puts "Currently serving #{array[0]}."
