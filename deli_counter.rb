680a15957e0cb8a1052debd0f42fa911f68bb74d
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 04:17:02 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+katz_deli=[]
+
+def line(array)
+line = []
+if array.length == 0 
+  puts "The line is currently empty."
+elsif array.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+      puts "The line is currently: #{line_array.join (" ")}
+    end
+
+end
