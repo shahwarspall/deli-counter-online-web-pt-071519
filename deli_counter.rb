a834005b450e0a6d2c774602019eb4fbf0333d27
Shahwar Spall <shevy.spall@gmail.com>
2019-07-20 05:13:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -20,7 +20,8 @@ def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{array[0]}."
     array.shift
+    puts "Currently serving #{array[0]}."
+    
   end
 end
